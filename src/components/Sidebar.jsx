3b6404b67f997f690a8fea63724f1030f5ad02a3
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-06-26 10:15:43 +0100

    Youtube clone project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { Stack } from '@mui/material';
+import React from "react";
+import { Stack } from "@mui/material";
 
-import { categories } from '../utils/constants'
+import { categories } from "../utils/constants";
 
-const selectedCategory = "New"
-
-const Sidebar = () => {
-  return (
-    <Stack
-      direction="row"
-      sx={{
-        overflowY: 'auto',
-        height: {sx: 'auto', md: '95%'},
-        flexDirection: {md: 'column'},
-      }}
-    >
-
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
     {categories.map((category) => (
       <button
-        className='category-btn'
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
-          background: category.name === selectedCategory && '#fc1503',
-          color: 'white'
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
         }}
-        key= {category.name}
+        key={category.name}
       >
-        <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
-        <span style={{opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
       </button>
     ))}
-    </Stack>
-  )
-}
+  </Stack>
+);
 
-export default Sidebar
+export default Categories;
